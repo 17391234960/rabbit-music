ff1d0463d424165530946a5cda0db8acf8bc7711
zhangy01 <706716160@qq.com>
2022-03-30 12:58:05 +0800

    修改默认Controller

--- src/main/java/com/rabbit/music/controller/DefaultController.java
+++ src/main/java/com/rabbit/music/controller/DefaultController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DefaultController {
 
 
-    @RequestMapping("/")
+    @RequestMapping("")
     public String test() {
         return "say hello";
     }
